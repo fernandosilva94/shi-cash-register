bad590a967bed984f2746a0d53caacce22d1e1f9
Fernando Silva <fernandoadmti@hotmail.com>
2023-06-14 22:03:49 -0300

    criando consulta por nome no repository de usuario

--- src/main/java/com/shi/shicash/repository/UsuarioRepository.java
+++ src/main/java/com/shi/shicash/repository/UsuarioRepository.java
@@ -1,8 +1,13 @@
 package com.shi.shicash.repository;
 
 import com.shi.shicash.models.UsuarioModel;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {}
+public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+    List<UsuarioModel> findByNomeLike(String nome);
+}
